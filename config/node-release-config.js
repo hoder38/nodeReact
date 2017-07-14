f251f09f4a9df44ff1a572b2064dcc1064f798de
hoder38 <hoder3388@gmail.com>
2017-07-14 14:03:24 +0800

    bugfix

--- config/node-release-config.js
+++ config/node-release-config.js
@@ -3,36 +3,38 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-var EXTENT_IP = exports.EXTENT_IP = 'www.anomopi.com';
-var EXTENT_FILE_IP = exports.EXTENT_FILE_IP = 'www.anomopi.com';
-var IP = exports.IP = '192.168.2.100';
-var FILE_IP = exports.FILE_IP = '192.168.2.101';
-var PORT = exports.PORT = 443;
-var EXTENT_PORT = exports.EXTENT_PORT = 443;
-var FILE_PORT = exports.FILE_PORT = 443;
-var EXTENT_FILE_PORT = exports.EXTENT_FILE_PORT = 3388;
-var COM_PORT = exports.COM_PORT = 3390;
-var WS_PORT = exports.WS_PORT = 3388;
-var DB_NAME = exports.DB_NAME = 'releasedb';
-var DB_IP = exports.DB_IP = '192.168.2.101';
-var DB_PORT = exports.DB_PORT = 27017;
-var SESS_IP = exports.SESS_IP = '192.168.2.101';
-var SESS_PORT = exports.SESS_PORT = 6379;
-var GOOGLE_MEDIA_FOLDER = exports.GOOGLE_MEDIA_FOLDER = '0B_BstyDfOj4Rfk54SHNvU2ExV3RJTGlvWHljWTRHSXprcU0tM0o5QXR6c1dfWlNiVTV6VlE';
-var GOOGLE_BACKUP_FOLDER = exports.GOOGLE_BACKUP_FOLDER = '0B_BstyDfOj4RfmJqUkJINGpVS0RhMm1qNlR5WE9FTlo2RUVQY3RIOXhlY3RRU0Vtb2NaeWc';
-var NAS_TMP = exports.NAS_TMP = '/mnt/tmp';
-var NAS_PREFIX = exports.NAS_PREFIX = '/mnt/release/storage';
-var HINT = exports.HINT = true;
-var AUTO_UPLOAD = exports.AUTO_UPLOAD = true;
-var AUTO_DOWNLOAD = exports.AUTO_DOWNLOAD = true;
-var UPDATE_STOCK = exports.UPDATE_STOCK = true;
-var UPDATE_EXTERNAL = exports.UPDATE_EXTERNAL = true;
-var CHECK_MEDIA = exports.CHECK_MEDIA = true;
-var API_LIMIT = exports.API_LIMIT = 10;
-var TORRENT_LIMIT = exports.TORRENT_LIMIT = 5;
-var MEGA_LIMIT = exports.MEGA_LIMIT = 1;
-var ZIP_LIMIT = exports.ZIP_LIMIT = 1;
-var STREAM_LIMIT = exports.STREAM_LIMIT = 300;
-var STOCK_DATE = exports.STOCK_DATE = [14, 15, 22, 23, 29, 30, 7, 8];
-var STOCK_MODE = exports.STOCK_MODE = 1;
-//3: xml重抓 2: xml重parse 1: parse tag
+exports.default = {
+    EXTENT_IP: 'www.anomopi.com',
+    EXTENT_FILE_IP: 'www.anomopi.com',
+    IP: '192.168.2.100',
+    FILE_IP: '192.168.2.101',
+    PORT: 443,
+    EXTENT_PORT: 443,
+    FILE_PORT: 443,
+    EXTENT_FILE_PORT: 3388,
+    COM_PORT: 3390,
+    WS_PORT: 3388,
+    DB_NAME: 'releasedb',
+    DB_IP: '192.168.2.101',
+    DB_PORT: 27017,
+    SESS_IP: '192.168.2.101',
+    SESS_PORT: 6379,
+    GOOGLE_MEDIA_FOLDER: '0B_BstyDfOj4Rfk54SHNvU2ExV3RJTGlvWHljWTRHSXprcU0tM0o5QXR6c1dfWlNiVTV6VlE',
+    GOOGLE_BACKUP_FOLDER: '0B_BstyDfOj4RfmJqUkJINGpVS0RhMm1qNlR5WE9FTlo2RUVQY3RIOXhlY3RRU0Vtb2NaeWc',
+    NAS_TMP: '/mnt/tmp',
+    NAS_PREFIX: '/mnt/release/storage',
+    HINT: true,
+    AUTO_UPLOAD: true,
+    AUTO_DOWNLOAD: true,
+    UPDATE_STOCK: true,
+    UPDATE_EXTERNAL: true,
+    CHECK_MEDIA: true,
+    API_LIMIT: 10,
+    TORRENT_LIMIT: 5,
+    MEGA_LIMIT: 1,
+    ZIP_LIMIT: 1,
+    STREAM_LIMIT: 300,
+    STOCK_DATE: [14, 15, 22, 23, 29, 30, 7, 8],
+    STOCK_MODE: 1,
+    //3: xml重抓 2: xml重parse 1: parse tag
+}
