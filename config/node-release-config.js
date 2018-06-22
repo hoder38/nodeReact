2801571d92bc267ee4312dc7cf327a03ac57740b
hoder38 <hoder3388@gmail.com>
2018-06-22 09:21:51 +0800

    bugfix

--- config/node-release-config.js
+++ config/node-release-config.js
@@ -38,7 +38,7 @@ exports.default = {
     TORRENT_LIMIT: 5,
     MEGA_LIMIT: 1,
     ZIP_LIMIT: 1,
-    STOCK_DATE: [13, 14, 20, 21, 27, 28, 6, 7],
+    STOCK_DATE: [8, 9, 15, 16, 22, 23, 1, 2],
     STOCK_MODE: 1,
     //3: xml重抓 2: xml重parse 1: parse tag
 }
