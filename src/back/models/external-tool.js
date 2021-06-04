cd5b74917daec5dd95ad4766b7c662474a555013
hoder38 <hoder3388@gmail.com>
2021-06-04 09:58:31 +0800

    bugfix

--- src/back/models/external-tool.js
+++ src/back/models/external-tool.js
@@ -1683,20 +1683,22 @@ export default {
                         })));
                     } else {
                         const sp = findTag(p, 'span')[0];
-                        const pcsp = findTag(sp)[0];
-                        if (pcsp && pcsp.match(/本文及附表/)) {
-                            const a = findTag(findTag(sp, 'strong')[0], 'a')[0];
-                            const url = a ? addPre(a.attribs.href, 'https://www.mof.gov.tw') : addPre(findTag(findTag(findTag(findTag(sp, 'span')[0], 'strong')[0], 'span')[0], 'a')[0].attribs.href, 'https://www.mof.gov.tw');
-                            driveName = `${obj.name} ${obj.date}${PathExtname(url)}`;
-                            console.log(driveName);
-                            return mkFolder(PathDirname(filePath)).then(() => Api('url', encodeURI(url), {filePath}).then(() => GoogleApi('upload', {
-                                type: 'auto',
-                                name: driveName,
-                                filePath,
-                                parent,
-                                rest: () => updateDocDate(type, obj.date),
-                                errhandle: err => handleError(err),
-                            })));
+                        if (sp) {
+                            const pcsp = findTag(sp)[0];
+                            if (pcsp && pcsp.match(/本文及附表/)) {
+                                const a = findTag(findTag(sp, 'strong')[0], 'a')[0];
+                                const url = a ? addPre(a.attribs.href, 'https://www.mof.gov.tw') : addPre(findTag(findTag(findTag(findTag(sp, 'span')[0], 'strong')[0], 'span')[0], 'a')[0].attribs.href, 'https://www.mof.gov.tw');
+                                driveName = `${obj.name} ${obj.date}${PathExtname(url)}`;
+                                console.log(driveName);
+                                return mkFolder(PathDirname(filePath)).then(() => Api('url', encodeURI(url), {filePath}).then(() => GoogleApi('upload', {
+                                    type: 'auto',
+                                    name: driveName,
+                                    filePath,
+                                    parent,
+                                    rest: () => updateDocDate(type, obj.date),
+                                    errhandle: err => handleError(err),
+                                })));
+                            }
                         }
                     }
                 };
