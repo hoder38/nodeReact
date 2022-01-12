52433a574047b775dc9e0277315c65bf1a23fc70
hoder38 <hoder3388@gmail.com>
2022-01-12 10:18:01 +0800

    checkstpck改為不等結果直接倒數

--- src/back/cmd/background.js
+++ src/back/cmd/background.js
@@ -224,11 +224,19 @@ export const dbBackup = () => {
 
 export const checkStock = () => {
     if (CHECK_STOCK(ENV_TYPE)) {
-        const checkS = () => {
+        /*const checkS = () => {
             console.log('checkStock');
             const newStr = (new Date().getHours() >= 20) ? true : false;
             return stockStatus(newStr).catch(err => bgError(err, 'Loop checkStock')).then(() => new Promise((resolve, reject) => setTimeout(() => resolve(), PRICE_INTERVAL * 1000))).then(() => checkS());
+        }*/
+        //怕程序死掉 改為不等執行完就開始倒數
+        const checkS = () => {
+            console.log('checkStock');
+            const newStr = (new Date().getHours() >= 20) ? true : false;
+            stockStatus(newStr).catch(err => bgError(err, 'Loop checkStock'));
+            return new Promise((resolve, reject) => setTimeout(() => resolve(), PRICE_INTERVAL * 1000)).then(() => checkS());
         }
+
         return new Promise((resolve, reject) => setTimeout(() => resolve(), 330000)).then(() => checkS());
     }
 }
