265a890206356e6f8ff0fbd732cd470d4575e7c1
hoder38 <hoder3388@gmail.com>
2022-06-17 10:10:56 +0800

    new background loop

--- src/back/cmd/background.js
+++ src/back/cmd/background.js
@@ -1,5 +1,5 @@
 import { ENV_TYPE } from '../../../ver.js'
-import { AUTO_UPLOAD, CHECK_MEDIA/*, UPDATE_EXTERNAL*/, AUTO_DOWNLOAD, UPDATE_STOCK, /*STOCK_MODE, STOCK_DATE, */STOCK_FILTER, DB_BACKUP, CHECK_STOCK, BITFINEX_LOAN, BITFINEX_FILTER, USSE_TICKER, TWSE_TICKER } from '../config.js'
+import { AUTO_UPLOAD, CHECK_MEDIA/*, UPDATE_EXTERNAL*/, AUTO_DOWNLOAD, UPDATE_STOCK, /*STOCK_MODE, STOCK_DATE, */STOCK_FILTER, DB_BACKUP, CHECK_STOCK, BITFINEX_LOAN, BITFINEX_FILTER, USSE_TICKER, TWSE_TICKER, MAX_RETRY } from '../config.js'
 import { DRIVE_INTERVAL, USERDB, MEDIA_INTERVAl, EXTERNAL_INTERVAL, DOC_INTERVAL, /*STOCK_INTERVAL, */STOCKDB, BACKUP_COLLECTION, BACKUP_INTERVAL, PRICE_INTERVAL, RATE_INTERVAL, FUSD_SYM, SUPPORT_COIN, SUPPORT_PAIR } from '../constants.js'
 import Mongo from '../models/mongo-tool.js'
 import StockTool, { getStockListV2, getSingleAnnual, stockStatus } from '../models/stock-tool.js'
@@ -23,6 +23,7 @@ let lastInitTwse = 0;
 let currentSetOffer = 0;
 let currentInitUsse = 0;
 let currentInitTwse = 0;
+let currentUpdateStockList = 0;
 
 function bgError(err, type) {
     sendWs(`${type}: ${err.message||err.msg}`, 0, 0, true);
@@ -113,12 +114,16 @@ export const updateStockList = () => {
         //怕程序死掉 改為不等執行完就開始倒數
         const loopUpdateStockList = () => {
             if (stock_batch_list.length > 0) {
-                console.log('loopUpdateStockList');
-                console.log(new Date().toLocaleString());
-                console.log(stock_batch_list[0]);
-                console.log(stock_batch_list.length);
-                const item = stock_batch_list.splice(0, 1);
-                StockTool.getSingleStockV2(item[0].type, item[0], 1).catch(err => bgError(err, 'Loop updateStockList'))
+                const now = Math.round(new Date().getTime() / 1000);
+                if (!currentUpdateStockList || currentUpdateStockList < (now - RATE_INTERVAL * MAX_RETRY)) {
+                    currentUpdateStockList = now;
+                    console.log('loopUpdateStockList');
+                    console.log(new Date().toLocaleString());
+                    console.log(stock_batch_list[0]);
+                    console.log(stock_batch_list.length);
+                    const item = stock_batch_list.splice(0, 1);
+                    StockTool.getSingleStockV2(item[0].type, item[0], 1).catch(err => bgError(err, 'Loop updateStockList')).then(() => currentUpdateStockList = 0);
+                }
             }
             return new Promise((resolve, reject) => setTimeout(() => resolve(), RATE_INTERVAL * 1000)).then(() => loopUpdateStockList());
         }
@@ -146,10 +151,10 @@ export const updateStock = () => {
             console.log(new Date().toLocaleString());
             //let use_stock_list = [];
             const sd = new Date();
-            const parseStockList = () => (sd.getDay() === 4 && sd.getHours() === 2) ? getStockListV2('twse', new Date().getFullYear(), new Date().getMonth() + 1).then(stocklist => {
+            const parseStockList = () => (sd.getDay() === 2 && sd.getHours() === 10) ? getStockListV2('twse', new Date().getFullYear(), new Date().getMonth() + 1).then(stocklist => {
                 stocklist.forEach(i => stock_batch_list.push(i));
                 //return updateStockList();
-            }) : (sd.getDay() === 5 && sd.getHours() === 2) ? getStockListV2('usse', new Date().getFullYear(), new Date().getMonth() + 1).then(stocklist => {
+            }) : (sd.getDay() === 5 && sd.getHours() === 10) ? getStockListV2('usse', new Date().getFullYear(), new Date().getMonth() + 1).then(stocklist => {
                 stocklist.forEach(i => stock_batch_list.push(i));
                 //return updateStockList();
             }) : Promise.resolve();
@@ -209,7 +214,6 @@ export const checkStock = () => {
             stockStatus(newStr).catch(err => bgError(err, 'Loop checkStock'));
             return new Promise((resolve, reject) => setTimeout(() => resolve(), PRICE_INTERVAL * 1000)).then(() => checkS());
         }
-
         return new Promise((resolve, reject) => setTimeout(() => resolve(), 330000)).then(() => checkS());
     }
 }
